2a7372ac7df55cfa882334d4d0deaed22a255bd4
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-10-17 01:50:01 -0300

    introducing numpy

---
+++ numpy/test.py
@@ -0,0 +1,15 @@
+import numpy as np
+print('--- Numpy ---')
+
+l = [1,2,3]
+l2 = np.array(l)
+print(type(l2))
+print(l2)
+print()
+
+m = [[1,2,3],[4,5,6],[7,8,9]]
+m2 = np.array(m)
+print(m2)
+print()
+
+print('--- Arrange ---')

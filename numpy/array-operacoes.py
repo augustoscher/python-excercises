98f2fea0e3542a5da9bb1becd295378514fcfeac
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-10-20 21:58:16 -0300

    operações vetorizadas

---
+++ numpy/array-operacoes.py
@@ -0,0 +1,9 @@
+import numpy as np
+print('--- Operações com Numpy arrays ---')
+
+# Operação vetorizada: Soma cada elemento com o segundo array
+array1 = np.arange(0, 20)
+print(array1)
+array2 = array1 + array1
+print()
+print(array2)

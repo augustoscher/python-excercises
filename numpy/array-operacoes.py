4e851a175ef216ac14b565d2e43fc5c2be168673
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-10-20 22:12:30 -0300

    function numpy

--- numpy/array-operacoes.py
+++ numpy/array-operacoes.py
@@ -15,3 +15,30 @@ array3 = array1 * array1
 print(array3)
 print()
 
+# Operações escalares: Soma 100 a cada elemento 
+array4 = array1 + 100
+print(array4)
+print()
+
+# Funções numpy
+array1 = np.arange(1, 11)
+print('Raiz quadrada dos elementos')
+print(np.sqrt(array1))
+
+print('Exponenciação dos elementos')
+print(np.exp(array1))
+
+print('Média dos elementos')
+print(np.mean(array1))
+
+print('Desvio padrão')
+print(np.std(array1))
+
+print('Seno dos elementos')
+print(np.sin(array1))
+
+print('Valor máximo')
+print(np.max(array1))
+
+print('Valor minimo')
+print(np.min(array1))

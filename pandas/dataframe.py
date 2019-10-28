8bba75aa632079bd466e77bf1139b115d1087eb8
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-10-27 23:00:37 -0300

    dataframes

--- pandas/dataframe.py
+++ pandas/dataframe.py
@@ -4,8 +4,13 @@ print('--- Pandas/Dataframes ---')
 
 np.random.seed(101)
 
-df = pd.DataFrame(np.random.randn(5, 4), 'A B C D E'.split(), 'W X Y Z'.split())
+df = pd.DataFrame(np.random.randn(5, 4), index='A B C D E'.split(), columns='W X Y Z'.split())
 print(df)
 print()
 
-# Consumo de dados de um Dataframe
+# Consumo/indexação de dados de um Dataframe
+
+# Busca os dados da coluna "W". Irá retornar uma serie
+s1 = df['W']
+print(type(s1))
+print(s1)

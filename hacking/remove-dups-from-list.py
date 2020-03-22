f60aa5c242b62cae823b3fbf404a6fc861be357e
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-03-22 16:22:22 -0300

    Python hacking

--- hacking/remove-dups-from-list.py
+++ hacking/remove-dups-from-list.py
@@ -1,11 +1,26 @@
+def remove_duplicated(l):
+  return list(dict.fromkeys(l))
+
 arr = [2, 3, 6, 6, 5]
-# remove dups items by using dictionary
-d = dict.fromkeys(arr)
-# print(d)
-
-#convert back to array
-arr = list(d)
+arr = remove_duplicated(arr)
 arr.sort(reverse = True)
-
 print(arr)
-print(arr[1])
+
+#-----------------------------
+print()
+scores = [37.21, 37.21, 37.2, 41, 39]
+scores2 = scores[:]
+
+#ordena, remove a nota mais baixa e busca a segunda menor
+scores2.sort()
+scores2 = scores2[1:]
+lowest = scores2[0]
+
+indexes = []
+i = 0
+for item in scores:
+  if item == lowest:
+    indexes.append(i)
+    i += 1
+
+print(indexes)

4bae98ea23674e2d5cfeec363ac9d68ada9d1444
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-03-22 16:49:39 -0300

    Python hacking

--- basics/basics.py
+++ basics/basics.py
@@ -16,3 +16,24 @@ print()
 nome = 'Teste'
 idade = 18
 print('Nome: {a}, Idade: {b}'.format(a=nome, b=idade))
+
+
+def print_full_name(a, b):
+    print('Hello {a} {b}! You just delved into python.'.format(a=a, b=b))
+
+print_full_name('Ross', 'Taylor')
+
+
+a = "this is a string"
+a = a.split(" ") # a is converted to a list of strings. 
+print(a)
+
+string = ''
+for item in a:
+    if len(string) > 0:
+        string = string +"-"+ item
+    else:
+        string = string + item
+        
+
+print(string)

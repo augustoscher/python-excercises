9e935476b6110385477b2028cb38f4accd86bc42
Augusto <augustoscher@gmail.com>
2023-11-03 16:14:50 -0300

    python tuples

--- basics/tuple.py
+++ basics/tuple.py
@@ -6,6 +6,20 @@ print(type(t))
 print(t[2])
 print()
 
+# adding values to tuple
+t = t + (7,)
+print(t)
+
+# appending values to tuple
+tuples = (2, 4, 6, 8, 12)
+tuples = (*tuples, 15)
+print(tuples)
+
+# insert values to the beggining of a tuple
+tuples2 = (2, 4, 6, 8, 12)
+tuples2 = (0, *tuples2)
+print(tuples2)
+
 #returns error if we try to modify some value
 #the code below throws error
 #t[1] = 'asdj';

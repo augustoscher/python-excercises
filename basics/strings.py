982a6012e7c2fdf7a1c64a94f9cfc4d796878a78
Augusto <augustoscher@gmail.com>
2024-02-05 08:51:31 -0300

    feat: python strings

--- basics/strings.py
+++ basics/strings.py
@@ -37,3 +37,8 @@ print(any(c.isalpha() for c in s))
 print(any(c.isdigit() for c in s))
 print(any(c.islower() for c in s))
 print(any(c.isupper() for c in s))
+
+
+
+width = 20
+print('HackerRank'.rjust(width,'-'))
